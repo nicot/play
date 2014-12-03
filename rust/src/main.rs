d74cb26a9b81fe4f34c73293aa341405652d11a1
Nico Tonozzi <nico@nicot.us>
2014-12-02 23:25:56 -0700

    simple tcp listener in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,39 @@
+#![feature(phase)]
+#[phase(plugin, link)] extern crate log;
+
+use std::io::{TcpListener, TcpStream};
+use std::io::{Acceptor, Listener};
+
+fn handle_client(mut stream: TcpStream) {
+    match stream.write(b"HELLO!") {
+        Ok(()) => info!("Response sent"),
+        Err(e) => warn!("Failed to respond {}", e),
+    }
+    let mut buf = [100];
+    let n = stream.read(&mut buf);
+    println!("{}: {}", n, buf);
+    drop(stream);
+}
+
 fn main() {
-    let x = 5i;
 
-    let y = if x == 5i { 10i } else { 15i };
+    let listener = TcpListener::bind("127.0.0.1:1080").unwrap();
 
-    println!("The value of x is : {}", x);
+    // bind the listener to the specified address
+    let mut acceptor = listener.listen();
+
+    // accept connections and process them, spawning a new tasks for each one
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(e) => { warn!("fail: {}", e); }
+            Ok(stream) => spawn(proc() {
+                // connection succeeded
+                info!("Successfully accepted TCP connection.");
+                handle_client(stream)
+            })
+        }
+    }
+
+    // close the socket server
+    drop(acceptor);
 }

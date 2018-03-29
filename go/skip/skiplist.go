84e4f02f16673a5351a2fe4d48fa81ba5884d5ca
Nico Tonozzi <code@nicot.us>
2018-03-29 08:06:24 -0600

    wip

--- go/skip/skiplist.go
+++ go/skip/skiplist.go
@@ -14,10 +14,40 @@ type Skiplist struct {
 
 // Set puts a key into the skiplist
 func (s *Skiplist) Set(key int, value interface{}) {
+	element := &skipList{
+		key,
+		value,
+		nil,
+		nil,
+	}
+	if s.underlying == nil {
+		s.underlying = element
+	}
 
 }
 
 // Get grabs a key from the skiplist
 func (s *Skiplist) Get(key int) interface{} {
-	return nil
+	v, present := get(s.underlying, key)
+	if !present {
+		panic("value not found")
+	}
+	return v
+}
+
+// Get grabs a key from the skiplist
+func get(s *skipList, key int) (interface{}, bool) {
+	if s == nil {
+		return nil, false
+	}
+	if s.key == key {
+		return key, true
+	}
+	if s.next == nil {
+		return get(s.down, key)
+	}
+	if key > s.next.key {
+		return get(s.next, key)
+	}
+	return get(s.down, key)
 }

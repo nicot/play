0a1e81bb6047ac37012ce6f6b11684155c6827d9
Nico Tonozzi <code@nicot.us>
2018-04-02 22:17:38 -0600

    wip

--- go/range_inversion.go
+++ go/range_inversion.go
@@ -7,15 +7,11 @@ type Range struct {
 	end int
 }
 
-func invert_ranges(ranges [][]]Range) []Range {
-	initial := []Range{
-			Range{
-			math.MinInt64,
-			math.MaxInt64,
-		}
+func invert_ranges(ranges [][]Range) []Range {
+	currentRange := []Range{
+		start: math.MinInt64,
+		end: math.MaxInt64,
 	}
-
-	currentRange := initial
 
 	for _, rv := range ranges {
 		currentRange = invert_range(currentRange, rv)
@@ -25,4 +21,33 @@ func invert_ranges(ranges [][]]Range) []Range {
 }
 
 func invert_range(range1 []Range, range2 []Range) []Range {
+	if len(range1) == 0 {
+		return range2
+	}
+	if len(range2) == 0 {
+		return range1
+	}
+
+	next := make([]Range{}, 0)
+	currentEnd := math.MaxInt64
+	i := 0
+	j := 0
+	for {
+		// todo length checks
+		var smaller Range
+		if range1[0].start < range2[0].start {
+			smaller = range1[i]
+			i++
+		} else {
+			smaller = range2[j]
+			j++
+		}
+		if smaller.end < currentEnd {
+			next[len(next) - 1].end = smaller.end
+		}
+
+		// actually do the inverting
+		next = append(next, smaller)
+		currentEnd = smaller.end
+	}
 }
